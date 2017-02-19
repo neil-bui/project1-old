a529c5e6a87155efff3c3a17080fcba8307979cf
rockygrocky <rorygreen@Rorys-MacBook-Pro.local>
2017-02-19 07:48:13 -0500

    polished code

--- wp-content/themes/swagtron/header.php
+++ wp-content/themes/swagtron/header.php
@@ -194,7 +194,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <li><a href="<?php echo get_term_link(52); ?>">Parts & Accessories</a>
             </li>
                         <li><a href="<?php echo get_term_link(76); ?>">CLEARANCE</a>
-              <ul  style="left:-60px">
+              <ul  style="left:-60px ">
                 <li><a href="<?php echo get_term_link(49); ?>">Clothing & Bags</a></li>
                 <li><a href="https://swagtron.com/product/1-year-accidental-damage-warranty/">Accidental Warranty</a></li>
                 <li><a href="<?php echo get_term_link(51); ?>">CLEARANCE ITEMS</a></li>
